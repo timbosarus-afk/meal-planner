fce4979597576e61321ec1612ab2fe12f2fe226d
timbosarus-afk <timbosarus@googlemail.com>
2026-08-03 21:00:32 +0100

    Fix missing escapeHtml function definition (was called but never defined after the persistence rewrite)

--- extension/popup.js
+++ extension/popup.js
@@ -125,5 +125,11 @@ async function restoreListIfAny() {
   }
 }
 
+function escapeHtml(str) {
+  const div = document.createElement('div');
+  div.textContent = str;
+  return div.innerHTML;
+}
+
 loadRecipes();
 restoreListIfAny();
